bf1eb266d43b15627c58cd8b078766e03f51ec77
Nima Thakare <nima.thakare@gmail.com>
2020-02-28 11:57:30 +0530

    Displaying Listview using Toast message

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -8,7 +8,11 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -16,18 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView myListView = findViewById(R.id.myListView);
-        final ArrayList<String> Country = new ArrayList<>();
-        Country.add("India");
-        Country.add("Japan");
-        Country.add("Australia");
-        Country.add("Indonesia");
-        Country.add("Turkey");
+        final ListView myListView = findViewById(R.id.myListView);
+        final ArrayList<String> Country = new ArrayList<>(asList ("India","Japan","Australia","Indonesia","Turkey"));
+
         /*Let’s assume you want to display a list in your Android app.
         For this you will use the ListView provided by Android.
         ListViews don’t actually contain any data themselves.
         It’s just a UI element without data in it. You can populate your ListViews by using an Android adapter
-
+        
         Adapter is an interface whose implementations provide data and control the display of that data.
         ListViews own adapters that completely control the ListView’s display.
         So adapters control the content displayed in the list as well as how to display it.
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                Log.i("Selected Country",Country.get(i));
+                Toast.makeText(MainActivity.this, "Good Morning " + Country.get(i), Toast.LENGTH_SHORT).show();
             }
         });
 
